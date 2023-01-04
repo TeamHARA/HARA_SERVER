b56f2898690ae980a0f5b549c05910813978c02a
Kayoung <rkdkdudjd@gmail.com>
2023-01-04 18:16:27 +0900

    [FIX] 기존 response 빌더 사용

--- src/common/error/handler.ts
+++ src/common/error/handler.ts
@@ -1,14 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import statusCode from "../../constants/statusCode";
 import responseMessage from "../../constants/responseMessage";
-
+import { fail } from "../../constants/response";
 const globalExceptionHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
   const status = err?.statusCode || statusCode.INTERNAL_SERVER_ERROR;
-  res.status(status).json({
+  res.status(status).send(fail(
     status,
-    success: false,
-    message: err?.message || responseMessage.INTERNAL_SERVER_ERROR
-  });
+    err?.message || responseMessage.INTERNAL_SERVER_ERROR
+  ));
   next(err);
 };
 
--- src/controller/worryWithController.ts
+++ src/controller/worryWithController.ts
@@ -1,16 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ClientException } from "../common/error/exceptions/customExceptions";
+import { success } from "../constants/response";
 import statusCode from "../constants/statusCode";
 import { worryWithService } from '../service';
-
-const sendOk = (res: Response, message: string, data?: any) => {
-  res.status(statusCode.OK).json({
-    status: statusCode.OK,
-    success: true,
-    message,
-    data
-  });
-}
 
 const updateFinalOption = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -19,7 +11,8 @@ const updateFinalOption = async (req: Request, res: Response, next: NextFunction
       throw new ClientException("필요한 값이 없습니다.");
     }
     await worryWithService.chooseFinalOption(userId, worryWithId, chosenOptionId);
-    sendOk(res, "나의고민 최종결정 성공");
+
+    res.status(statusCode.OK).send(success(statusCode.OK, "나의고민 최종결정 성공"));
   } catch (error) {
     next(error);
   }
