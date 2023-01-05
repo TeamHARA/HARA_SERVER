9856c4918cd68d3604dabfc8ba514f0fb6b1af5e
yujindonut <hyujin0825@gmail.com>
2023-01-05 20:48:19 +0900

    [FEAT] 고민글목록 전체조회 - controller
    고민글목록 전체조회 - controller

---
+++ src/controller/worryController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { fail, success } from "../constants/response";
+import { rm, sc } from "../constants";
+import { ClientException } from "../common/error/exceptions/customExceptions";
+import statusCode from "../constants/statusCode";
+import { worryService } from "../service";
+
+const getWorryListByCategory = async (req: Request, res: Response) => {
+    try {
+        const { categoryId } = req.params;
+
+        if (!categoryId) {
+            throw new ClientException("필요한 Param 값이 없습니다.");
+        }
+
+        const data = await worryService.getWorryListByCategoryId(+categoryId);
+
+        return res.status(sc.OK).send(success(statusCode.OK, rm.READ_WORRYLIST_SUCCESS, data));
+
+    } catch (error) {
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.READ_WORRYLIST_FAIL));
+    }
+};
+
+export default { getWorryListByCategory };
