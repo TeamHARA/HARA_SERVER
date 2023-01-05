04070b2ba13ede227fe55112f679e37c9600d851
yujindonut <hyujin0825@gmail.com>
2023-01-05 20:48:59 +0900

    [FEAT] 고민글 전체 조회 Repository 생성
    고민글 전체 조회 Repository 생성

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,4 +1,6 @@
 import worryWithRepository from './worryWithRepository';
 import withOptionRepository from './withOptionRepository';
+import categoryRepository from './categoryRepository';
+import worryRepository from './worryRepository';
 
-export { worryWithRepository, withOptionRepository };
+export { worryWithRepository, withOptionRepository, categoryRepository, worryRepository };
