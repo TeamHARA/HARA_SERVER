510df376ab4a0ad411bbef9cecd5979782017d24
yelin <yelin@kim-yelins-MacBook-Pro.local>
2023-01-05 19:20:16 +0900

    [FIX] worryAloneController 에서 에러처리 수정

--- src/controller/worryAloneController.ts
+++ src/controller/worryAloneController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import { rm, sc } from "../constants";
 import { fail, success } from "../constants/response";
 import worryAloneService from '../service/worryAloneService';
+import { validationResult } from 'express-validator';
 
 
 const uploadWorryAlone = async (req:Request, res:Response) => {
@@ -10,6 +11,16 @@ const uploadWorryAlone = async (req:Request, res:Response) => {
     const uploadWorryAloneDTO : UploadWorryAloneDTO = req.body;
     //console.log(uploadWorryAloneDTO);
 
+    //? validation의 결과를 바탕으로 분기 처리
+    const error = validationResult(req);
+    if(!error.isEmpty()) {
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST))
+    }
+  
+    if(!uploadWorryAloneDTO.categoryId || !uploadWorryAloneDTO.content || !uploadWorryAloneDTO.options || !uploadWorryAloneDTO.title || !uploadWorryAloneDTO.userId){
+        return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
+
+    }
 
     const data = await worryAloneService.uploadWorryAlone(uploadWorryAloneDTO);
 
