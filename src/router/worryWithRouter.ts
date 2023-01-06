7fd099edf8471b5f97ebe61babe4ba8ad18cda6a
csb9427 <csb9427@naver.com>
2023-01-07 03:37:43 +0900

    [FEAT] router 부분 구현

--- src/router/worryWithRouter.ts
+++ src/router/worryWithRouter.ts
@@ -1,11 +1,12 @@
-import { Router } from 'express';
-import { worryWithController } from '../controller';
-import { auth } from '../middlwares';
+import { Router } from "express";
+import { worryWithController } from "../controller";
+import { auth } from "../middlwares";
 
 const router = Router();
 
-router.patch('/', auth, worryWithController.updateFinalOption);
-router.post("/",auth, worryWithController.createWithWorry);
-router.get(":/worryId",auth, worryWithController.findWithWorry);
+router.patch("/", auth, worryWithController.updateFinalOption);
+router.post("/", auth, worryWithController.createWithWorry);
+router.get("/", auth, worryWithController.getWithWorry);
+router.get("/:worryId", auth, worryWithController.findWithWorry);
 
 export default router;
