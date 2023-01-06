5511e62735de4a66e11894948e59ae919943e01c
yujindonut <hyujin0825@gmail.com>
2023-01-06 16:46:36 +0900

    [FEAT] 고민글 투표하기 생성
    고민글 투표하기 생성
    - Repository
    - Router
    - Service
    - Controller

---
+++ src/service/voteService.ts
@@ -0,0 +1,27 @@
+import { ClientException } from "../common/error/exceptions/customExceptions";
+import statusCode from "../constants/statusCode";
+import { voteRepository, withOptionRepository, worryWithRepository } from "../repository"
+import { CreateVoteDTO } from "../createWorryVoteDTO";
+
+const createWorryVote = async (createVoteDTO: CreateVoteDTO) => {
+
+    const worryWith = await worryWithRepository.findById(createVoteDTO.worryWithId);
+    console.log(worryWith);
+
+    const worryOption = await withOptionRepository.findById(createVoteDTO.optionId);
+
+    if (!worryWith) {
+        throw new ClientException("해당하는 아이디의 걱정글이 존재하지 않습니다");
+    }
+
+    if (!worryOption) {
+        throw new ClientException("해당하는 아이디의 선택지가 존재하지 않습니다");
+    }
+
+    if (worryWith?.userId == createVoteDTO.userId) {
+        throw new ClientException("자신의 고민글에는 투표할 수 없습니다", statusCode.FORBIDDEN);
+    }
+    await voteRepository.createWorryVote(createVoteDTO);
+};
+
+export default { createWorryVote };
