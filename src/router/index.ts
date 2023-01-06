ab9052bfee4c9e166e8365558fde7ae3d6c68b01
csb9427 <csb9427@naver.com>
2023-01-06 15:38:37 +0900

    [FEAT] 라우터 및 DTO 설정

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import worryWithRouter from './worryWithRouter';
+import worryWithRouter from "./worryWithRouter";
+import randomRouter from "./randomRouter";
 
 const router: Router = Router();
 
-router.use('/worry/with', worryWithRouter);
+router.use("/worry/with", worryWithRouter);
+router.use("/random", randomRouter);
 
 export default router;
