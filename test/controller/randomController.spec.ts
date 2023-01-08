ada1cdbf4222c35596d64229006e95a841c8f200
yujindonut <hyujin0825@gmail.com>
2023-01-09 00:18:25 +0900

    [FEAT] 과거 고민 조회 테스트코드 작성
    과거 고민 조회 테스트코드 작성

--- test/controller/randomController.spec.ts
+++ test/controller/randomController.spec.ts
@@ -1,5 +1,8 @@
+import { PrismaClient } from '@prisma/client';
 import request from "supertest";
 import app from "../testApp";
+
+const prisma = new PrismaClient();
 
 describe("GET /random", () => {
   it("올바른 응답", async () => {
@@ -7,4 +10,12 @@ describe("GET /random", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.message).toEqual("랜덤 답변 조회 성공");
   });
+});
+
+describe("GET /random/list", () => {
+  it("올바른 응답", async () => {
+    const response = await request(app).get("/api/random/list");
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toEqual("과거고민 목록 조회 성공");
+  })
 });
