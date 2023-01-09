15a99d3df8e23cf050316f34dc3602e027bec8d3
yujindonut <hyujin0825@gmail.com>
2023-01-10 00:26:52 +0900

    [CHORE] prisma 코드 수정
    prisma 코드 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,5 +4,7 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 sudo /usr/bin/yarn
-sudo npx prisma generate
+
+sudo /usr/bin/yarn db:pull 
+sudo /usr/bin/yarn generate 
 sudo /usr/bin/pm2 start dist
