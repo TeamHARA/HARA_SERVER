6a8588d60c726412e6365cac148c04040652096e
yujindonut <hyujin0825@gmail.com>
2023-01-10 16:48:08 +0900

    [CHORE] 코드 수정
    - 명령어 코드 수정
    - 명령어 주석 한글로 추가

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,7 +3,16 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-sudo /usr/bin/yarn
+#? dependencies 설치
+echo 'Installing Dependencies ...'
+sudo yarn
 sudo /usr/bin/yarn db:pull 
-sudo /usr/bin/yarn generate 
-sudo /usr/bin/pm2 start dist
+
+#? Prisma 사전 작업
+echo 'Generating Prisma ...'
+yarn generate 
+
+#? PM2로 서버 실행
+echo 'Starting server with PM2 ...'
+sudo pm2 start dist
+
