a283c6405f64b1106074890e06c8c3da85c7eb5f
csb9427 <csb9427@naver.com>
2023-01-10 17:35:01 +0900

    [FIX] 혼자 고민 목록 조회 수정

--- src/controller/worryAloneController.ts
+++ src/controller/worryAloneController.ts
@@ -6,7 +6,7 @@ import { validationResult } from "express-validator";
 import { ClientException } from "../common/error/exceptions/customExceptions";
 import { worryAloneService } from "../service";
 import statusCode from "../constants/statusCode";
-import { getFormattedDate } from '../common/utils/dateFormat';
+import { getFormattedDate } from "../common/utils/dateFormat";
 import { voteRepository } from "../repository";
 
 const postAloneWorry = async (
@@ -23,19 +23,21 @@ const postAloneWorry = async (
         .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
     }
 
-
-    const createdAloneWorry = await worryAloneService.createAloneWorry(createAloneWorryDTO);
+    const createdAloneWorry = await worryAloneService.createAloneWorry(
+      createAloneWorryDTO
+    );
 
     if (!createdAloneWorry) {
-        return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.CREATE_WORRY_ALONE_ERROR))
+      return res
+        .status(sc.NOT_FOUND)
+        .send(fail(sc.NOT_FOUND, rm.CREATE_WORRY_ALONE_ERROR));
     }
 
     res.status(sc.OK).send(success(sc.OK, rm.CREATE_WORRY_ALONE_SUCCESS));
-
-    } catch (error) {
-        next(error);
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getAloneWorry = async (
   req: Request,
@@ -43,8 +45,8 @@ const getAloneWorry = async (
   next: NextFunction
 ) => {
   try {
-    const { ifSolved } = req.body;
-    const aloneWorries = await worryAloneService.readAloneWorry(ifSolved);
+    const { ifSolved } = req.params;
+    const aloneWorries = await worryAloneService.readAloneWorry(+ifSolved);
     res
       .status(sc.OK)
       .send(success(sc.OK, rm.READ_ALONEWORRY_SUCCESS, aloneWorries));
@@ -53,39 +55,54 @@ const getAloneWorry = async (
   }
 };
 
-const patchAloneWorry = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const reqValidationResult = validationResult(req);
-        if (!reqValidationResult.isEmpty()) {
-            return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
-        }
-
-        const { userId, worryAloneId, chosenOptionId } = req.body;
-
-        await worryAloneService.chooseFinalOption({
-            userId,
-            aloneWorryId: worryAloneId,
-            chosenOptionId,
-        })
-
-        res.status(sc.OK).send(success(sc.OK, rm.CHOOSE_ALONE_OPTION_SUCCESS));
-    } catch (error) {
-        next(error);
+const patchAloneWorry = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const reqValidationResult = validationResult(req);
+    if (!reqValidationResult.isEmpty()) {
+      return res
+        .status(sc.BAD_REQUEST)
+        .send(fail(sc.BAD_REQUEST, rm.NULL_VALUE));
     }
-}
 
-const getAloneWorryDetail =async (req:Request, res: Response, next: NextFunction) => {
+    const { userId, worryAloneId, chosenOptionId } = req.body;
+
+    await worryAloneService.chooseFinalOption({
+      userId,
+      aloneWorryId: worryAloneId,
+      chosenOptionId,
+    });
+
+    res.status(sc.OK).send(success(sc.OK, rm.CHOOSE_ALONE_OPTION_SUCCESS));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAloneWorryDetail = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { aloneWorryId } = req.params;
     const { userId } = req.body;
-    
+
     if (!aloneWorryId) {
       throw new ClientException("필요한 파라미터 값이 없습니다.");
     }
 
-    const gotWithWorryDetail = await worryAloneService.findAloneWorryDetail(+aloneWorryId,userId);
-    const options = await worryAloneService.findOptionsAloneWorryId(+aloneWorryId);
-   
+    const gotWithWorryDetail = await worryAloneService.findAloneWorryDetail(
+      +aloneWorryId,
+      userId
+    );
+    const options = await worryAloneService.findOptionsAloneWorryId(
+      +aloneWorryId
+    );
+
     const result = {
       finalOption: gotWithWorryDetail.finalOption,
       createdAt: getFormattedDate(gotWithWorryDetail.createdAt),
@@ -93,19 +110,19 @@ const getAloneWorryDetail =async (req:Request, res: Response, next: NextFunction
       worryContent: gotWithWorryDetail.content,
       category: gotWithWorryDetail.content,
       options: options,
-    }
+    };
 
-    res.status(statusCode.OK).send(success(statusCode.OK, "혼자고민 상세조회 성공", result));
+    res
+      .status(statusCode.OK)
+      .send(success(statusCode.OK, "혼자고민 상세조회 성공", result));
   } catch (error) {
     next(error);
   }
 };
 
-
-
 export default {
-    postAloneWorry,
-    getAloneWorry,
-    patchAloneWorry,
-    getAloneWorryDetail,
-}
+  postAloneWorry,
+  getAloneWorry,
+  patchAloneWorry,
+  getAloneWorryDetail,
+};
