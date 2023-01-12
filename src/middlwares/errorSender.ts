5780e4a4062587336c5fe3486a5e4398faf7ced5
Kayoung <rkdkdudjd@gmail.com>
2023-01-13 00:16:26 +0900

    [FIX] 서버 에러만 슬랙에 보내도록 수정

--- src/middlwares/errorSender.ts
+++ src/middlwares/errorSender.ts
@@ -1,21 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import statusCode from "../constants/statusCode";
 import slackAlarm, { SlackMessageFormat } from "./slackAlarm";
 
-const sendErrorStackToSlack = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  const error = err as Error;
-  const message: SlackMessageFormat = {
-    color: slackAlarm.colors.danger,
-    title: "해라 서버 에러",
-    text: "실패",
-    fields: [
-      {
-        title: "Error Stack:",
-        value: `\`\`\`${error.stack}\`\`\``, //여기서 ```를 추가해서 마크다운 형태로 보내줍니다.
-      },
-    ],
-  };
-  slackAlarm.sendMessage(message);
-  next(err);
+const sendErrorStackToSlack = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err?.statusCode == statusCode.INTERNAL_SERVER_ERROR) {
+    const message: SlackMessageFormat = {
+      color: slackAlarm.colors.danger,
+      title: "해라 서버 에러",
+      text: "실패",
+      fields: [
+        {
+          title: "Error Stack:",
+          value: `\`\`\`${err.stack}\`\`\``,
+        },
+      ],
+    };
+    slackAlarm.sendMessage(message);
+  }
+  
 };
 
 export default { sendErrorStackToSlack };
