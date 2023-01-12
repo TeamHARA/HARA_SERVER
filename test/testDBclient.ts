8c7679c16d5243e3397268793929c375cff69657
Kayoung <rkdkdudjd@gmail.com>
2023-01-13 04:56:35 +0900

    [TEST] orm을 모킹 e2e 테스트 - 혼자 고민 결정하기

---
+++ test/testDBclient.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
+import prisma from '../src/repository/prismaClient';
+
+jest.mock('../src/repository/prismaClient', () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}))
+
+beforeEach(() => {
+  mockReset(prismaMock)
+})
+
+export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
